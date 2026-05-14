b2c4c26aea6d5b4a19b57f0f9bf3a95f754e59d6
singhayush61 <ayushofficialsv@gmail.com>
2026-05-15 05:26:14 +0530

    fix: update CI/CD pipeline to include Node.js setup and testing steps

--- app/server.js
+++ app/server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Project Running");
-  res.send("CI/CD Working");
+   res.send("CI/CD Working");
 });
 
 app.listen(3000, () => {

c299c3b2c508c1efa37db8dc607e386b5fe2ae6e
Ayush Singh <160009745+singhayush61@users.noreply.github.com>
2026-05-22 04:31:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,16 +24,3 @@ resource "aws_instance" "devops_server" {
               sudo usermod -aG docker ubuntu
               EOF
 }
-
-resource "time_sleep" "wait_5_minutes" {
-  depends_on = [aws_instance.devops_server]
-  create_duration = "5m"
-}
-
-resource "null_resource" "destroy_trigger" {
-  depends_on = [time_sleep.wait_5_minutes]
-
-  triggers = {
-    instance_id = aws_instance.devops_server.id
-  }
-}
